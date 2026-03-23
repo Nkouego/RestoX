4e75251a4e6635db74608ad48aba8037b8e99435
Nkouego <larissankouegodev@gmail.com>
2026-03-23 10:04:01 +0100

    Initial commit

---
+++ backend/src/main/java/com/laraim237/restoX/RestoXApplication.java
@@ -0,0 +1,13 @@
+package com.laraim237.restoX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoXApplication.class, args);
+	}
+
+}
